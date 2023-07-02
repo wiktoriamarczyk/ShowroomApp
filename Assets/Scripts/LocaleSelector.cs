2712c471b9090ade3e8fb50f7a7b238d77d06945
wmarczyk <wiktoria.marczyk@itsilesia.com>
2023-07-02 17:46:17 +0200

    Start working on localization

---
+++ Assets/Scripts/LocaleSelector.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+public class LocaleSelector : MonoBehaviour
+{
+    private bool active = false;
+    int index = 1;
+
+    public void ChangeLocale() {
+        if (active == true)
+            return;
+
+        index++;
+        int localeID = Mathf.Abs(index) % LocalizationSettings.AvailableLocales.Locales.Count;
+        StartCoroutine(SetLocale(localeID));
+    }
+
+    IEnumerator SetLocale(int localeID) {
+        active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeID];
+        active = false;
+    }
+}
